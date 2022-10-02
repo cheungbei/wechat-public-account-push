1a2283bdf541aa241efa44c3bbe6e5ac7c826e92
cheungbei <114331208+cheungbei@users.noreply.github.com>
2022-10-02 12:00:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ok9Em6k7tl1iblSl81Ym1Mv65sbA',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '	x8iGY-SwWAKhN98Q4tcvzIn27KgotTydYXLIBnVAz8Y',
+      useTemplateId: 'iMmJ_jrWYSl4S23htvJAhfsjQf3WvusBwDF1nhK1UEU',
       // 所在省份
       province: '山西',
       // 所在城市
